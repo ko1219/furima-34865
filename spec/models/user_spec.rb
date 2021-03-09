914d2c8ae429701b22c4e58f915ebe79b491f7cf
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-09 11:14:12 +0900

    ログインテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -108,21 +108,46 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
-  describe 'ユーザーログイン' do
-    it 'password,emailが記入されていればログインできる' do
-      expect(@user).to be_valid
+end
+RSpec.describe 'ログイン', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインができるとき' do
+    it '保存されているユーザーの情報と合致すればログインができる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # 正しいユーザー情報を入力する
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # トップページへ遷移することを確認する
+      expect(current_path).to eq(root_path)
+      # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
-    it 'passwordが空だとログインできない'do
-      @user.password = ""
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Invalid Email or password.
-        ")
-    end
-    it 'emailが空だとログインできない'do
-      @user.email = ""
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Invalid Email or password.
-        ")
+  end
+  context 'ログインができないとき' do
+    it '保存されているユーザーの情報と合致しないとログインができない' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
+      # ログインページへ遷移する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'email', with: ''
+      fill_in 'password', with: ''
+      # ログインボタンを押す
+      find('input[name="commit"]').click
+      # ログインページへ戻されることを確認する
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 end
