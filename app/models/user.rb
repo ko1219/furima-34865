ac37fac83e3178c3422367c8832b0f576ca9f312
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-09 18:21:00 +0900

    itemsテーブル・Activestorageの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  with_options presence: true
+  with_options presence: true do
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/  , message: 'には全角文字を使用してください' }
@@ -12,3 +12,4 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
   end
+end
