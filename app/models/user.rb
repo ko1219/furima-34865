8648ae9c97641037df93ece8a0a5bbe52e2e2e0d
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-09 16:34:41 +0900

    validates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' } do
-    validates :first_name
-    validates :last_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/  , message: 'には全角文字を使用してください' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
-  validates :nickname, presence: true
-  validates :birthday, presence: true
-end
+  with_options presence: true
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/  , message: 'には全角文字を使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/  , message: 'には全角文字を使用してください' }
+    validates :nickname
+    validates :birthday
+  end
