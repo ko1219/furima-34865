b6c6cec4c52a8959839310150a149e8cb93a2698
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-10 12:24:07 +0900

    createアクション作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
+  def item_params
+    params.require(:item).permit(:name, :description,:image, :category_id, :prefecture_id, :shipping_charge_id, :shipping_day_id, :status_id, :price).merge(user_id: current_user.id)
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_registration_path
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,25 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
-  belongs_to :shippingcharge
+  belongs_to :shipping_charge
   belongs_to :prefecture
-  belongs_to :shippingdate
+  belongs_to :shipping_day
   belongs_to :status
   has_one_attached :image
   belongs_to :user
 
 with_options presence: true do
+  validates :image
   validates :name
   validates :description
   validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' },
-  format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  inclusion: { in: 300..9999999, message: 'Out of setting range' }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_charge_id, numericality: { other_than: 1 }
   validates :shipping_day_id, numericality: { other_than: 1 }
   validates :status_id, numericality: { other_than: 1 }
   validates :category_id, numericality: { other_than: 1 }
   validates :user_id
-  validates :image, attached_file_presence: true
 end
 
 end
