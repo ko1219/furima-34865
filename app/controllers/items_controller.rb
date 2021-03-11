0b83b26c7b8f2a19685cee5447620d004f8eecc2
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-11 19:00:38 +0900

    トップページへのリダイレクト編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new, :edit]
-  before_action :move_to_index, except: [:index, :show]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -24,6 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -40,9 +42,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description,:image, :category_id, :prefecture_id, :shipping_charge_id, :shipping_day_id, :status_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
