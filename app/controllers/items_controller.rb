c39c116c00a802ae9ebcc806bc0ccb11f60a9185
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-11 18:41:32 +0900

    ログインページへの遷移修正後

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,6 +41,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
