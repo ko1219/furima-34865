954d25e4e8e12faecec0d900cdef775f9dc130aa
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-11 16:58:40 +0900

    updateアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,12 @@ class ItemsController < ApplicationController
   end
 
   def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
   end
 
   private
