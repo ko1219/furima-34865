36989c1b9c4f2eb4ddae256fcf9c7c9fd64f2ff6
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-11 19:07:22 +0900

    editアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
