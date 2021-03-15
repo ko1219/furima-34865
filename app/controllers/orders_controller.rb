28c27f9e459f7ed99c5ea77fc7a2dfe69368d4b7
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-15 16:03:36 +0900

    indexアクション、パラメーター設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,14 @@
 class OrdersController < ApplicationController
   def index
+    @order_address = OrderAddress.new(params[:id])
   end
 
   def create
+    @order_address = OrderAddress.create(order_address_params)
+  end
+
+  private
+  def order_address_params
+    params.permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number, :price, :item_id, :user_id)
   end
 end
