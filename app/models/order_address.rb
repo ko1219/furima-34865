23602befd56406b6fde3872332b57712c9dbf3b2
ko1219 <73377107+ko1219@users.noreply.github.com>
2021-03-23 18:53:39 +0900

    売却済みの商品にsold outo を表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,9 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :postcode, :prefecture_id, :city, :block, :building, :phone_number, :item_id, :user_id, :token, :price, :order_id
 
-  VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0-?\d{4}-?\d{4}\z/
+  VALID_PHONE_NUMBER_REGEX = /\A\d{10}\z/
+                             /\A\d{11}\z/
+
   VALID_POSTCODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/
   with_options presence: true do
     validates :postcode, format: { with: VALID_POSTCODE_REGEX, message: 'Input correctly' }
